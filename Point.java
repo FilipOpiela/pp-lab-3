1a06ab487dd5301594fa5641ab2e49e0c77d706b
FilipOpiela <162892006+FilipOpiela@users.noreply.github.com>
2024-04-07 12:45:43 +0200

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,26 @@
+class Point {
+	
+	private double x;
+	private double y;
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setX(int z) {
+		x = z;
+	}
+	
+	public void setY(int z) {
+		y = z;
+	}
+	
+	Point(){
+		x = 0;
+		y=0;
+	}	
+}
