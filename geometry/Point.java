9af49026f95f1bb38af419334393a9f7f69c4c73
FilipOpiela <162892006+FilipOpiela@users.noreply.github.com>
2024-04-07 13:03:59 +0200

    Add files via upload

---
+++ geometry/Point.java
@@ -0,0 +1,27 @@
+package geometry;
+public class Point {
+	
+	private double x;
+	private double y;
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setX(int z) {
+		x = z;
+	}
+	
+	public void setY(int z) {
+		y = z;
+	}
+	
+	public Point(){
+		x = 0;
+		y = 0;
+	}
+}
